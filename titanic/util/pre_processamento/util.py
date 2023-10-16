ebb7b6e5895c6aa9693ae154c9ae093701aedc19
gabrielemannue <gabriel.emannuel.gama.andrade@ccc.ufcg.edu.br>
2023-10-16 00:41:37 -0300

    Refatorando Módulos de Pré_processamento

--- titanic/util/pre_processamento/util.py
+++ titanic/util/pre_processamento/util.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as pd
 from sklearn.base import TransformerMixin
+from sklearn.preprocessing import StandardScaler
+from sklearn.experimental.enable_iterative_imputer import IterativeImputer
 
 class CategorizarParaDummies(TransformerMixin):
     def __init__(self) -> None:
@@ -48,6 +51,51 @@ class GerarDummies(TransformerMixin):
                   y: None = None) -> pd.DataFrame:
         return pd.get_dummies(X)
     
+class Normalizador(TransformerMixin):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__normalizador = StandardScaler()
+
+    def fit(self, 
+            X: pd.DataFrame, 
+            y: None = None) -> pd.DataFrame:    
+        self.__normalizador.fit(X)
+        return self
+
+    def transform(self, 
+                  X: pd.DataFrame,
+                  y: None = None) -> pd.DataFrame:
+        X_transformado = pd.DataFrame(self.__normalizador.transform(X))
+        manter_colunas_nomes(X.columns, X_transformado)
+        X_transformado.index = X.index
+        return X_transformado
+    
+class PreencherValoresVazios(TransformerMixin):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__preenche = IterativeImputer()
+    
+    def fit(self,
+            X: pd.DataFrame,
+            y: None = None) -> pd.DataFrame:
+        self.__preenche.fit(X)
+        return self
+    
+    def transform(self,
+                  X: pd.DataFrame,
+                  y: None = None) -> pd.DataFrame:
+        X_transformado = pd.DataFrame(self.__preenche.transform(X))
+        manter_colunas_nomes(X.columns, X_transformado)
+        X_transformado.index = X.index
+        return X_transformado
+    
+def manter_colunas_nomes(colunas_originais: np.ndarray,
+                         X_transformado: pd.DataFrame):
+    for index_coluna in range(colunas_originais.shape[0]):
+        X_transformado.rename(columns={
+            index_coluna: colunas_originais[index_coluna]
+        }, inplace=True)
+
 def definir_faixa_etaria(idade: int | None) -> str:
         if 12 >= idade >= 0:
             return 'criança'
